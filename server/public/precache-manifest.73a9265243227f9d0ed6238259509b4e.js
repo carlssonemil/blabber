8859c55cef6bccfca65bdfac5fba8053c5e8f23f
carlssonemil <hello@emilcarlsson.se>
2020-05-15 17:52:11 +0200

    Remove /server/public folder from .gitignore

---
+++ server/public/precache-manifest.73a9265243227f9d0ed6238259509b4e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0353f5f78644a2bed433",
+    "url": "/css/app.5a6b584d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "dc18013d79d746d1833149a5e5881d2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a94a50687b22449d410",
+    "url": "/js/about.a609a9bd.js"
+  },
+  {
+    "revision": "0353f5f78644a2bed433",
+    "url": "/js/app.abf47036.js"
+  },
+  {
+    "revision": "a282c086180248720e23",
+    "url": "/js/chunk-vendors.5e9d70fa.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
