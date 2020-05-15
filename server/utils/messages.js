4b77f7f329d2027f54b618a247468ad206dfd390
carlssonemil <hello@emilcarlsson.se>
2020-05-15 18:44:04 +0200

    Fix bug with message object

--- server/utils/messages.js
+++ server/utils/messages.js
@@ -1,11 +1,11 @@
 function formatMessage(user, message, type) {
   if (!type) type = 'message';
-  
+
   return {
     user,
     message: {
       type: message.attachment && !message.message ? 'attachment' : type,
-      content: message.attachment ? message.message : message,
+      content: message.message,
       attachment: message.attachment || null
     },
     timestamp: new Date()
